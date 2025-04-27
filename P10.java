5620f1a27407e7af77f55d410fdbd56cc03c9f82
AsmaaElawady <asmaalwdy6@gmail.com>
2025-04-27 18:34:41 +0300

    Added P10

---
+++ P10.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.util.*;
+
+public class P10 {
+    int[] states = {0, 1, 2}; // 0: after x or start, 1: after y, 2: dead state
+    int[] startState = {0};
+    int[] finalState = {0, 1}; // Accept if in state 0 or 1
+    char[] alphabet = {'E', 'x', 'y'}; // Epsilon not used
+
+    Map<Integer, int[][]> transitionTable = Map.ofEntries(
+        Map.entry(0, new int[][]{{-1}, {0}, {1}}), // from state 0
+        Map.entry(1, new int[][]{{-1}, {0}, {2}}), // from state 1
+        Map.entry(2, new int[][]{{-1}, {2}, {2}})  // dead state
+    );
+
+    NFA nfa = new NFA(states, startState, finalState, alphabet, transitionTable);
+
+    // public P10(BufferedReader br, BufferedWriter bw) {
+    //     nfa.solveProblem(br, bw);
+    // }
+
+    public P10(List<String> testCases, BufferedWriter bw) {
+        nfa.solveProblem(testCases, bw);
+        System.out.println("Problem 10 done");
+
+    }
+}
