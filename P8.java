f41047bba1653eaf3abcfb5e3307f18e6414de75
AsmaaElawady <asmaalwdy6@gmail.com>
2025-04-27 19:19:18 +0300

    Update Problem 8

--- P8.java
+++ P8.java
@@ -9,11 +9,11 @@ int[] finalState = {4};
 char[] alphabet = {'E','a', 'b'}; // Alphabet with 'E' for epsilon, 'a', and 'b'
 
 Map<Integer, int[][]> transitionTable = Map.ofEntries(
-    Map.entry(0, new int[][]{{0, 1}, {0, 1}, {0}}),   
-    Map.entry(1, new int[][]{{-1}, {2}, {-1}}),      
-    Map.entry(2, new int[][]{{3}, {2, 3}, {2}}),      
-    Map.entry(3, new int[][]{{-1}, {4}, {-1}}),       
-    Map.entry(4, new int[][]{{4}, {4}, {4}})          
+    Map.entry(0, new int[][]{{-1}, {0, 1}, {0}}),   
+    Map.entry(1, new int[][]{{-1}, {-1}, {2}}),      
+    Map.entry(2, new int[][]{{-1}, {2, 3}, {2}}),      
+    Map.entry(3, new int[][]{{-1}, {-1}, {4}}),       
+    Map.entry(4, new int[][]{{-1}, {4}, {4}})          
 );
 
 NFA nfa = new NFA(states,startState,finalState,alphabet,transitionTable);
