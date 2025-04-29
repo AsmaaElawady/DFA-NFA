4b4d7cee19829686b92960ebe54b9e0bd042665f
salma ameer <salmaameer409@gmail.com>
2025-04-29 15:42:01 +0300

    Added p4 & p9 Drawings

--- P7.java
+++ P7.java
@@ -3,7 +3,7 @@ import java.util.List;
 import java.util.Map;
 
 public class P7 {
-    int[] states = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    int[] states = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int[] startState = {0};
     int[] finalState = {4, 5, 7, 8, 9, 10};
     char[] alphabet = {'E', '0', '1'}; 
@@ -12,16 +12,14 @@ public class P7 {
         Map.entry(0, new int[][]{{-1}, {1}, {2}}),     
         Map.entry(1, new int[][]{{-1}, {3}, {4}}),     
         Map.entry(2, new int[][]{{-1}, {5}, {6}}),     
-        Map.entry(3, new int[][]{{-1}, {11}, {4}}),   
+        Map.entry(3, new int[][]{{-1}, {-1}, {4}}),   
         Map.entry(4, new int[][]{{-1}, {7}, {8}}),   
         Map.entry(5, new int[][]{{-1}, {9}, {10}}),    
-        Map.entry(6, new int[][]{{-1}, {5}, {11}}),    
+        Map.entry(6, new int[][]{{-1}, {5}, {-1}}),    
         Map.entry(7, new int[][]{{-1}, {9}, {4}}),   
-        Map.entry(8, new int[][]{{-1}, {7}, {11}}),  
+        Map.entry(8, new int[][]{{-1}, {7}, {-1}}),  
         Map.entry(9, new int[][]{{-1}, {11}, {10}}),  
-        Map.entry(10, new int[][]{{-1}, {5}, {8}}) ,
-        Map.entry(11, new int[][]{{-1}, {11}, {11}})   // dead state
-    );
+        Map.entry(10, new int[][]{{-1}, {5}, {8}})     );
 
     NFA nfa = new NFA(states, startState, finalState, alphabet, transitionTable);
 
